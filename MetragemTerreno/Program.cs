c49d8ef5d8ad3c664eccc3bd99120f79e3e27e42
FrancieleAires <f.aires2016@gmail.com>
2023-01-12 19:28:38 -0300

    Adicionar arquivos de projeto.

---
+++ MetragemTerreno/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MyProject;
+using System.Globalization;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double largura, altura, area, preco, precoMetroQuadrado;
+
+        Console.WriteLine("Digite a largura do terreno: ");
+        altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite a altura do terreno: ");
+        largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Valor do metro quadrado do terreno:");
+        precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        area = largura * altura;
+        preco = area * precoMetroQuadrado;
+
+        Console.WriteLine("A área do terreno é: " + area.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("O preço do terreno é: " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        Console.ReadKey();
+    }
+}
+
+
